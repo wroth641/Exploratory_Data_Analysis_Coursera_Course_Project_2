a734849902c6403fd4a23d173509dccd301b1043
wroth641 <wroth@verisk.com>
2016-12-08 17:15:36 -0500

    Update Plot6.R

--- Plot6.R
+++ Plot6.R
@@ -33,4 +33,5 @@ g <- g + geom_bar(stat="identity")  +
   ylab(expression('Total PM'[2.5]*" Emissions")) +
   ggtitle('Total Emissions from motor vehicle (type=ON-ROAD) in Baltimore City, MD (fips = "24510") vs Los Angeles, CA (fips = "06037")  1999-2008')
 print(g)
+
 dev.off()
