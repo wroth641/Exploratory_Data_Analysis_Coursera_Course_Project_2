89d8cbf2d0f14237bbe4fbe7ffc89502ade82638
wroth641 <wroth@verisk.com>
2016-12-08 17:10:57 -0500

    Update Plot2.R

--- Plot2.R
+++ Plot2.R
@@ -15,4 +15,5 @@ aggregatedTotalByYear <- aggregate(Emissions ~ year, subsetNEI, sum)
 
 png('plot2.png')
 barplot(height=aggregatedTotalByYear$Emissions, names.arg=aggregatedTotalByYear$year, xlab="years", ylab=expression('total PM'[2.5]*' emission'),main=expression('Total PM'[2.5]*' in the Baltimore City, MD emissions at various years'))
+
 dev.off()
