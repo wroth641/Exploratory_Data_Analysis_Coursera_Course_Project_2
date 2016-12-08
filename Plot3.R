dc8928cb208f7b89790a11a37ff92dd2e8835582
wroth641 <wroth@verisk.com>
2016-12-08 17:14:38 -0500

    Update Plot3.R

--- Plot3.R
+++ Plot3.R
@@ -27,4 +27,5 @@ g <- g + geom_line() +
   ylab(expression('Total PM'[2.5]*" Emissions")) +
   ggtitle('Total Emissions in Baltimore City, Maryland (fips == "24510") from 1999 to 2008')
 print(g)
+
 dev.off()
