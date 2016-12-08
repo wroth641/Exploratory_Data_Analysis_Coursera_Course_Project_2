7999e3c575e71328ec7f6a114ba9887ed5769e8b
wroth641 <wroth@verisk.com>
2016-12-08 17:14:52 -0500

    Update Plot4.R

--- Plot4.R
+++ Plot4.R
@@ -30,4 +30,5 @@ g <- g + geom_bar(stat="identity") +
   ylab(expression('Total PM'[2.5]*" Emissions")) +
   ggtitle('Total Emissions from motor vehicle (type = ON-ROAD) in Baltimore City, Maryland (fips = "24510") from 1999 to 2008')
 print(g)
+
 dev.off()
